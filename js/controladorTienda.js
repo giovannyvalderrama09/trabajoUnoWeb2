19d8f4639908eaff6193ad896a06404f8376d713
203 <203@academicos.cesde.edu.co>
2022-02-16 10:14:18 -0500

    se crea v1 del proyecto

---
+++ js/controladorTienda.js
@@ -0,0 +1,35 @@
+// ENTRADAS DEL PROBLEMA (Comentario de linea)
+/* 
+Comentario 
+en 
+bloque
+*/ 
+
+//Entradas== Variables== Constantes 
+
+
+let nombreProducto = 'carro red bull' 
+let precioProducto = 400000
+let pesoProducto = 18.5
+let tieneDescuento = true
+let descripcionProducto = "carro a escala"
+let telefonoVendedor = "3128197508864"
+
+
+const NOMBRE_CLIENTE = "Giovanny"
+
+//Arreglos unidemensionales (vectores)
+// Sirven para almacenar varios elementos en una sola variable
+
+let productos=["carro red bull, camiseta, gorra, taza"]
+let productos2=Array("carro red bull, camiseta, gorra, taza")
+
+//SALIDAS 
+
+console.log("Buenas")
+console.log(nombreProducto)
+console.log("Su nombre es: " + NOMBRE_CLIENTE + "y su producto es "+ nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es ${nombreProducto}` )
+console.log("********")
+console.log(productos)
+console.log(productos2 [0])
